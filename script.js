bce9d83d14bb8a0e558951718517fb873663e7aa
dimasokov <92539686+m1rty@users.noreply.github.com>
2022-06-23 17:44:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-/*burger = document.querySelector('')*/
+let rate = document.querySelector('.trending__rating')
+let star = document.querySelector('.star')
+
+function main(){
+    if(document.rate.style.display  == "flex"){
+        document.star.style.display == "none"
+    }
+    else {
+        document.rate.style.backgroundColor == "blue"
+    }
+}
+
+
 
 
     
